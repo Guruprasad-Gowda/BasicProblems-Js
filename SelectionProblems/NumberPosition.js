a5d4db3567814b6e66b10bf1d02690020182b96a
NamaskaraGuru <guru8953@gmail.com>
2023-02-04 00:24:03 +0530

    Added: codes for Selection Practice Problems using if & else

---
+++ SelectionProblems/NumberPosition.js
@@ -0,0 +1,13 @@
+let number = 100;
+
+if (number == 1) {
+  console.log("Unit");
+} else if (number == 10) {
+  console.log("Ten");
+} else if (number == 100) {
+  console.log("Hundred");
+} else if (number == 1000) {
+  console.log("Thousand");
+} else {
+  console.log("Invalid number. Please enter a number (1, 10, 100, 1000, etc)");
+}
